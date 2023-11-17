9d212704b6fbee6d1c1361d792abcb1062b2739c
Jungscharjensemann <andrefl69216@gmail.com>
2023-11-17 12:36:12 +0100

    Unnütze Methoden entfernt, wichtige Methoden besser kommentiert.

--- src/main/java/de/swa/gc/GraphCode.java
+++ src/main/java/de/swa/gc/GraphCode.java
@@ -1,15 +1,13 @@
 package de.swa.gc;
 
-import java.io.Serializable;
-import java.util.*;
-import java.util.stream.Collectors;
-
 import com.google.common.collect.*;
-import com.google.common.primitives.Ints;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
-import org.apache.poi.ss.formula.functions.T;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /** Implementation of a Graph Code as a 2D representation of MMIR features 
  * 
@@ -92,66 +90,26 @@ public class GraphCode implements Serializable {
 //		System.out.println(idx_a + " " + idx_b + " (" + term1 + "), (" + term2 + ") -> " + val);
 	}
 
-	public Multimap<String, String> getTermsWithValue(int value) {
-		Multimap<String, String> multiMap = ArrayListMultimap.create();
-		for(int i = 0; i < dictionary.size(); i++) {
-			for(int j = 0; j < dictionary.size(); j++) {
-				if(matrix[i][j] == value) {
-					multiMap.put(dictionary.get(i), dictionary.get(j));
-				}
-			}
-		}
-		return multiMap;
-	}
-	
 	/** returns a JSon representation of this Graph Cocde **/
 	public String toString() {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String s = gson.toJson(this);
-		return s;
+		return gson.toJson(this);
 	}
 
+	@SuppressWarnings("unused")
 	public String printGC(boolean pretty) {
 		Gson gson = pretty ? new GsonBuilder().setPrettyPrinting().create() : new Gson();
 		return gson.toJson(this);
 	}
 
-	public String formattedTerms() {
-		TreeSet<Integer> uniqueValues = new TreeSet<>();
-		for (int[] ints : matrix) {
-			for (int anInt : ints) {
-				uniqueValues.add(anInt);
-			}
-		}
-		String formattedTerms = uniqueValues.stream()
-				.map(this::formattedTerms)
-				.collect(Collectors.joining(","));
-		return formattedTerms;
-	}
-
-	public String formattedTerms2() {
-		TreeSet<Integer> uniqueValues = new TreeSet<>();
-		for(int[] ints : matrix) {
-			for(int i : ints) {
-				uniqueValues.add(i);
-			}
-		}
-
-		TreeMultimap<Integer, Multimap<Integer, Integer>> treeMultimap;
-		for(int i = 0; i < matrix.length; i++) {
-			for(int j = 0; j < matrix[i].length; j++) {
-				// Diagonale ignorieren
-				if(i != j) {
-					Multimap<Integer, Integer> m = ArrayListMultimap.create();
-				}
-			}
-		}
-
-		// TreeSet<Einzigartiger Wert, ArrayList(MultiMap<Index des Terms, Werte>)>
-		return null;
-	}
-
-	public String getFormattedTerms2() {
+	/**
+	 * Fügt die Wörter in einem Wörterbuch und die zwischen
+	 * diesen bestehenden Beziehungen in einem
+	 * bestimmten Format zusammen.
+	 * Dieses Format lautet: <i_t> - <i_t1, i_t2, ..., i_tn>
+	 * @return Format.
+	 */
+	public String getFormattedTerms() {
 		List<String> formats = new ArrayList<>();
 		for(int row = 0; row < matrix.length; row++) {
 			int[] rowArr = matrix[row];
@@ -159,6 +117,8 @@ public class GraphCode implements Serializable {
 			// 1. Schlüssel (0,1,...)
 			// 2. Reihenfolge des Hinzufügens
 			Multimap<Integer, String> m = MultimapBuilder.treeKeys().arrayListValues().build();
+			// Für jeden einzigartigen Beziehungswert in einer Zeile,
+			// die entsprechenden Wörter sammeln.
 			for(int column = 0; column < rowArr.length; column++) {
 				if(column != row) {
 					int entry = rowArr[column];
@@ -169,7 +129,9 @@ public class GraphCode implements Serializable {
 			}
 			List<String> formatsInRow = new ArrayList<>();
 			int finalRow = row;
+			// Format als String formatieren.
 			m.asMap().forEach((k, v) -> {
+				// Beziehungswert nur 1.
 				if(k == 1) {
 					formatsInRow.add(String.format("<%s> - <%s>",
 							finalRow + 1,
@@ -177,7 +139,9 @@ public class GraphCode implements Serializable {
 									.map(s -> String.valueOf(dictionary.indexOf(s) + 1))
 									.collect(Collectors.joining(",")))
 					);
-				} else if(k > 1) {
+				}
+				// Beziehungswert alles außer 1 und positiv.
+				else if(k > 1) {
 					formatsInRow.add(String.format("<%s> %s <%s>",
 							finalRow + 1,
 							k,
@@ -191,142 +155,20 @@ public class GraphCode implements Serializable {
 				formats.add(String.join(",", formatsInRow));
 			}
 		}
+		// Formate für jede Zeile in einem einzigen Format zusammenfügen.
 		return !formats.isEmpty() ? String.join(",", formats) : "";
 	}
 
-	public String getFormattedTerms() {
-		Table<String, String, Integer> table = ArrayTable.create(dictionary, dictionary);
-		for(String s : dictionary) {
-			for(String t : dictionary) {
-				table.put(s, t, getEdgeValueForTerms(s, t));
-			}
-		}
-
-		// Für jeden Eintrag (Reihe) durchlaufen...
-		// Dann sollte für jede Reihe die darin vorkommenden einzigartigen Werte gesammelt werden (TreeSet)
-		// Für jeden einzigartigen Wert dann Einträge sammeln...
-		// Nach Werten differenzen (wenn 1, dann -, sonst <...> Wert <...,...,...>
-		// Diese gesammelten Einträge dann verbinden
-		// Format erstellen
-		// Formate verbinden...
-
-		Set<Map.Entry<String, Map<String, Integer>>> entries = table.rowMap().entrySet();
-		/*for(Map.Entry<String, Map<String, Integer>> row : entries) {
-			String key = row.getKey();
-			Map<String, Integer> columnEntry = row.getValue();
-			// Einzigartige Werte bestimmen...
-			TreeSet<Integer> uniqueValues = new TreeSet<>(columnEntry.values());
-
-			for(Integer i : uniqueValues) {
-				List<Map.Entry<String, Integer>> um = columnEntry.entrySet()
-						.stream()
-						.filter(column -> !column.getKey().equalsIgnoreCase(key)
-										&& Objects.equals(column.getValue(), i))
-						.collect(Collectors.toList());
-				if(Objects.equals(i, 1)) {
-					String format = String.format(
-							"<%s> - <%s>", dictionary.indexOf(key) + 1, um.stream()
-									.map(a -> String.valueOf(dictionary.indexOf(a.getKey()) + 1))
-									.collect(Collectors.joining(","))
-					);
-					sb.append(format);
-				} else if(i > 1){
-					if(!um.isEmpty()) {
-						String format = String.format(
-								"<%s> %s <%s>", dictionary.indexOf(key) + 1, i, um.stream()
-										.map(a -> String.valueOf(dictionary.indexOf(a.getKey()) + 1))
-										.collect(Collectors.joining(","))
-						);
-						sb.append(format);
-					}
-				}
-			}
-
-			String formats = uniqueValues.stream().map(i -> {
-				List<Map.Entry<String, Integer>> um = columnEntry.entrySet()
-						.stream()
-						.filter(column -> !column.getKey().equalsIgnoreCase(key)
-								&& Objects.equals(column.getValue(), i))
-						.collect(Collectors.toList());
-				if(Objects.equals(i, 1)) {
-					return String.format(
-							"<%s> - <%s>", dictionary.indexOf(key) + 1, um.stream()
-									.map(a -> String.valueOf(dictionary.indexOf(a.getKey()) + 1))
-									.collect(Collectors.joining(","))
-					);
-				} else if(i > 1){
-					if(!um.isEmpty()) {
-						return String.format(
-								"<%s> %s <%s>", dictionary.indexOf(key) + 1, i, um.stream()
-										.map(a -> String.valueOf(dictionary.indexOf(a.getKey()) + 1))
-										.collect(Collectors.joining(","))
-						);
-					}
-				}
-				return null;
-			}).filter(s -> s != null && !s.isEmpty())
-					.collect(Collectors.joining(","));
-			//System.out.println(formats);
-		}*/
-
-		return entries.stream().map(row -> {
-			String key = row.getKey();
-			Map<String, Integer> columnEntry = row.getValue();
-			TreeSet<Integer> uniqueValues = new TreeSet<>(columnEntry.values());
-			return uniqueValues.stream().map(i -> {
-						List<Map.Entry<String, Integer>> um = columnEntry.entrySet()
-								.stream()
-								.filter(column -> !column.getKey().equalsIgnoreCase(key)
-										&& Objects.equals(column.getValue(), i))
-								.collect(Collectors.toList());
-						if(Objects.equals(i, 1)) {
-							return String.format(
-									"<%s> - <%s>", dictionary.indexOf(key) + 1, um.stream()
-											.map(a -> String.valueOf(dictionary.indexOf(a.getKey()) + 1))
-											.collect(Collectors.joining(","))
-							);
-						} else if(i > 1){
-							if(!um.isEmpty()) {
-								return String.format(
-										"<%s> %s <%s>", dictionary.indexOf(key) + 1, i, um.stream()
-												.map(a -> String.valueOf(dictionary.indexOf(a.getKey()) + 1))
-												.collect(Collectors.joining(","))
-								);
-							}
-						}
-						return "";
-					}).filter(s -> s != null && !s.isEmpty())
-					.collect(Collectors.joining(","));
-		})
-				.filter(s -> !s.isEmpty())
-				.collect(Collectors.joining(","));
-	}
-
-	public String formattedTerms(int value) {
-		Multimap<String, String> m = getTermsWithValue(value);
-		Map<String, Collection<String>> map = m.asMap();
-
-
-		/*return map.entrySet().
-				stream().
-				map(entry -> String.format(
-						"<%s> - <%s>",
-						entry.getKey(),
-						String.join(",", entry.getValue())))
-				.collect(Collectors.joining(","));*/
-		return map.entrySet().
-				stream().
-				map(entry -> String.format(
-						"<%s> - <%s>",
-						dictionary.indexOf(entry.getKey()) + 1,
-						entry.getValue().stream().map(t -> String.valueOf(dictionary.indexOf(t) + 1)).collect(Collectors.joining(","))))
-				.collect(Collectors.joining(","));
-	}
-
+	/**
+	 * Zählt alle Wörter im Wörterbuch auf und
+	 * fügt diese mit einem Komma getrennt, zusammen.
+	 * @return Zusammengefügtes Wörterbuch.
+	 */
 	public String listTerms() {
 		return String.join(",", dictionary);
 	}
 
+	@SuppressWarnings("unused")
 	public String reducedString() {
 		Gson gson = new Gson();
 		JsonElement jsonElement = gson.fromJson(gson.toJson(this), JsonElement.class);
